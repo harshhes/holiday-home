bdc35602dde59bbd4853036aab2e9f752b7c9162
Harsh <harshgolu82@gmail.com>
2023-05-17 14:43:53 +0530

    minor changes

--- holiday_homes/settings.py
+++ holiday_homes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-i_trp0)u7gu68fdu&!rptyihjbz43u(*3hfg=6#)3@(gx7!=bt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 AUTH_USER_MODEL = 'base.Owner'
